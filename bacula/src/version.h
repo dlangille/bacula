2c0ec701e30f4deaa44178cbac7f871873ab1a46
Kern Sibbald <kern@sibbald.com>
2017-06-10 16:33:41 +0200

    Update version + date

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -5,9 +5,9 @@
 #define COMMUNITY 1      /* Define to create a Windows community binary */
 
 /* Note: there can be only *one* VERSION in this file */
-#define VERSION "7.9.3"
-#define BDATE   "07 June 2017"
-#define LSMDATE "07Jun17"
+#define VERSION "7.9.4"
+#define BDATE   "10 June 2017"
+#define LSMDATE "10Jun17"
 
 #define RELEASE 1   /* Use ONLY in rpms */
 
