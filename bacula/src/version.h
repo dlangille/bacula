8fe9f4a0a6b7a45e1f85288bf00adee1f98baa43
Kern Sibbald <kern@sibbald.com>
2017-07-10 20:18:13 +0200

    Tweak update version + date

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -5,9 +5,9 @@
 #define COMMUNITY 1      /* Define to create a Windows community binary */
 
 /* Note: there can be only *one* VERSION in this file */
-#define VERSION "9.0.0"
-#define BDATE   "02 July 2017"
-#define LSMDATE "02Jul17"
+#define VERSION "9.0.1"
+#define BDATE   "10 July 2017"
+#define LSMDATE "10Jul17"
 
 #define RELEASE 1   /* Use ONLY in rpms */
 
