7fd5aedcc41c9cabb33581146abd92326c9fd4c6
Kern Sibbald <kern@sibbald.com>
2017-07-13 16:35:11 +0200

    Tweak update version date

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -6,8 +6,8 @@
 
 /* Note: there can be only *one* VERSION in this file */
 #define VERSION "9.0.1"
-#define BDATE   "10 July 2017"
-#define LSMDATE "10Jul17"
+#define BDATE   "12 July 2017"
+#define LSMDATE "12Jul17"
 
 #define RELEASE 1   /* Use ONLY in rpms */
 
