1f5b92091ccc7c8ced19274f6da7d1ab6f917be6
Kern Sibbald <kern@sibbald.com>
2019-02-04 13:06:49 +0100

    Update version date for release

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -6,8 +6,8 @@
 
 /* Note: there can be only *one* VERSION in this file */
 #define VERSION "9.4.2"
-#define BDATE   "28 January 2019"
-#define LSMDATE "28Jan19"
+#define BDATE   "04 February 2019"
+#define LSMDATE "04Feb19"
 
 #define RELEASE 1   /* Use ONLY in rpms */
 
