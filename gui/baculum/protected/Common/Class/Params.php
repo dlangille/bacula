d7558cf31a845ddc52345be16cdfb52ed7571ce0
Marcin Haba <marcin.haba@bacula.pl>
2019-10-04 06:33:09 +0200

    baculum: Tweak update version number

--- gui/baculum/protected/Common/Class/Params.php
+++ gui/baculum/protected/Common/Class/Params.php
@@ -24,7 +24,7 @@ Prado::using('Application.Common.Class.CommonModule');
 
 class Params extends CommonModule {
 
-	const BACULUM_VERSION = '11.0.0';
+	const BACULUM_VERSION = '9.6.0';
 
 	public static $months = array(
 		'jan' => 'January',
