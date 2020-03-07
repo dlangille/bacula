d696d0758ba7c50b6a2cac617882f62e8cd106b2
Kern Sibbald <kern@sibbald.com>
2020-03-07 18:40:45 +0100

    Update version + date

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -5,9 +5,9 @@
 #define COMMUNITY 1      /* Define to create a Windows community binary */
 
 /* Note: there can be only *one* VERSION in this file */
-#define VERSION "9.6.2"
-#define BDATE   "26 February 2020"
-#define LSMDATE "26Feb20"
+#define VERSION "9.6.3"
+#define BDATE   "07 March 2020"
+#define LSMDATE "07Mar20"
 
 #define RELEASE 1   /* Use ONLY in rpms */
 
