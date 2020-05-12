54fbcfe2656187f9c0408e57d30e46e2e058a610
Git Backport Process <git@bacula.org>
2020-05-12 22:32:14 +0200

    BEE Backport bacula/src/fileopts.h
    This commit is the result of the squash of the following main commits:
    
    Author: Eric Bollengier <eric@baculasystems.com>
    Date:   Thu Dec 19 11:07:42 2019 +0100
    
        win32: Fix #5801 about incorrect File=/ behavior with subvolumes in backup and estimate jobs
    
    Author: Eric Bollengier <eric@baculasystems.com>
    Date:   Fri Dec 13 15:32:38 2019 +0100
    
        win32: Handle Exclude{} options when computing the drive list with File=/
    
    Author: Eric Bollengier <eric@baculasystems.com>
    Date:   Mon Oct 29 16:42:29 2018 +0100
    
        Fix issue between FO_PORTABLE and FO_PORTABLE_DATA
    
    Author: Alain Spineux <alain@baculasystems.com>
    Date:   Sun Apr 6 20:05:35 2014 +0200
    
        Beginning of dedup code
    
    Author: Kern Sibbald <kern@sibbald.com>
    Date:   Thu Mar 13 16:41:12 2014 +0100
    
        Update year in copyrights
    
    Author: Laurent Papier <papier@tuxfan.net>
    Date:   Wed May 18 23:25:30 2011 +0200
    
        Add LZO compression support in bacula-fd.
    
        Add new generic compressed streams This should make easier to
        add additional compression scheme
    
        Rename a few define, add an element containing compression algorithm
        in some data structures.
    
    Author: Kern Sibbald <kern@sibbald.com>
    Date:   Wed Nov 24 14:29:33 2010 +0100
    
        Define new stream bits
    
    Author: Kern Sibbald <kern@sibbald.com>
    Date:   Wed Nov 24 11:14:02 2010 +0100
    
        Separate definitions of streams, filetypes, and fileoptions to separate files

--- bacula/src/fileopts.h
+++ bacula/src/fileopts.h
@@ -1,18 +1,20 @@
 /*
-   Bacula® - The Network Backup Solution
+   Bacula(R) - The Network Backup Solution
 
-   Copyright (C) 2001-2014 Bacula Systems SA
-   All rights reserved.
+   Copyright (C) 2000-2020 Kern Sibbald
 
-   The main author of Bacula is Kern Sibbald, with contributions from many
-   others, a complete list can be found in the file AUTHORS.
+   The original author of Bacula is Kern Sibbald, with contributions
+   from many others, a complete list can be found in the file AUTHORS.
 
-   Licensees holding a valid Bacula Systems SA license may use this file
-   and others of this release in accordance with the proprietary license
-   agreement provided in the LICENSE file.  Redistribution of any part of
-   this release is not permitted.
+   You may use this file and others of this release according to the
+   license defined in the LICENSE file, which includes the Affero General
+   Public License, v3.0 ("AGPLv3") and some additional permissions and
+   terms pursuant to its AGPLv3 Section 7.
 
-   Bacula® is a registered trademark of Kern Sibbald.
+   This notice must be preserved when any source code is
+   conveyed and/or propagated.
+
+   Bacula(R) is a registered trademark of Kern Sibbald.
 */
 /*
  * File types 
