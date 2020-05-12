fd822ee9e7d284fdc3023c8e99910b02e9724123
Eric Bollengier <eric@baculasystems.com>
2020-05-12 19:07:23 +0200

    BEE Backport bacula/src/stored/org_libsd_quota.c
    This commit is the result of the squash of the following main commits:
    
    Author: Eric Bollengier <eric@baculasystems.com>
    Date:   Fri Mar 6 16:25:13 2020 +0100
    
        Move MaxPoolBytes checks in bee files

---
+++ bacula/src/stored/org_libsd_quota.c
@@ -0,0 +1,31 @@
+/*
+   Bacula(R) - The Network Backup Solution
+
+   Copyright (C) 2000-2020 Kern Sibbald
+
+   The original author of Bacula is Kern Sibbald, with contributions
+   from many others, a complete list can be found in the file AUTHORS.
+
+   You may use this file and others of this release according to the
+   license defined in the LICENSE file, which includes the Affero General
+   Public License, v3.0 ("AGPLv3") and some additional permissions and
+   terms pursuant to its AGPLv3 Section 7.
+
+   This notice must be preserved when any source code is
+   conveyed and/or propagated.
+
+   Bacula(R) is a registered trademark of Kern Sibbald.
+*/
+
+#include "bacula.h"
+#include "stored.h"
+
+bool dir_get_pool_info(DCR *dcr, VOLUME_CAT_INFO *volcatinfo)
+{
+   return true;
+}
+
+bool is_pool_size_reached(DCR *dcr, bool quiet)
+{
+   return false;
+}
