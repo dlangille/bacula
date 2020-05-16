423a64a2e2e2d8186b05401283ff47c879d5344d
Henrique <henrique.faria@baculasystems.com>
2020-05-16 11:37:44 +0200

    BEE Backport bacula/src/qt-console/tray-monitor/fd-config-ui-controller.h
    This commit is the result of the squash of the following main commits:
    
    Author: Henrique <henrique.faria@baculasystems.com>
    Date:   Wed Jun 26 12:03:27 2019 -0300
    
        android: added missing license and a few comments on src files. FD debug level reduced to 50
    
    Author: Henrique <henrique.faria@baculasystems.com>
    Date:   Mon May 27 21:07:25 2019 -0300
    
        android: added missing files for fd config screen

---
+++ bacula/src/qt-console/tray-monitor/fd-config-ui-controller.h
@@ -0,0 +1,48 @@
+﻿/*
+   Bacula(R) - The Network Backup Solution
+
+   Copyright (C) 2000-2020 Kern Sibbald
+
+   The original author of Bacula is Kern Sibbald, with contributions
+   from many others, a complete list can be found in the file AUTHORS.
+
+   You may use this file and others of this release according to the
+   license defined in the LICENSE file, which includes the Affero General
+   Public License, v3.0 ("AGPLv3") and some additional permissions and
+   terms pursuant to its AGPLv3 Section 7.
+
+   This notice must be preserved when any source code is
+   conveyed and/or propagated.
+
+   Bacula(R) is a registered trademark of Kern Sibbald.
+*/
+
+#ifndef FDCONFIGUICONTROLLER_H
+#define FDCONFIGUICONTROLLER_H
+
+#include <QObject>
+#include <QString>
+#include <fstream>
+#include "conf.h"
+#include "tray_conf.h"
+#include "resmodel.h"
+#include "config-storage.h"
+
+class FdConfigUiController : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit FdConfigUiController(QObject *parent = nullptr);
+    ~FdConfigUiController();
+
+public slots:
+   //FD Config File
+   void readFileDaemonConfig();
+   void writeFileDaemonConfig(QString fcontents);
+
+signals:
+  void fileDaemonConfigRead(const QString &text);
+};
+
+#endif // FDCONFIGUICONTROLLER_H
