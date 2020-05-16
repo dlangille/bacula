640492f5175bb9fd6ba22e7feceb27e94c8ae93a
Henrique <henrique.faria@baculasystems.com>
2020-05-16 11:42:40 +0200

    BEE Backport bacula/src/qt-console/tray-monitor/tray-ui-controller.cpp
    This commit is the result of the squash of the following main commits:
    
    Author: Henrique <henrique.faria@baculasystems.com>
    Date:   Tue Oct 15 18:17:37 2019 -0300
    
        android: tweak removed debug message
    
    Author: Henrique <henrique.faria@baculasystems.com>
    Date:   Sun Sep 1 23:07:59 2019 -0300
    
        android: added watcher to file daemon trace file
    
    Author: Henrique <henrique.faria@baculasystems.com>
    Date:   Fri Aug 16 16:15:21 2019 -0300
    
        android: fixed app crash while starting
    
    Author: Henrique <henrique.faria@baculasystems.com>
    Date:   Tue Aug 13 21:04:53 2019 -0300
    
        android: created enterprise and community versions
    
    Author: Henrique <henrique.faria@baculasystems.com>
    Date:   Tue Aug 13 16:07:23 2019 -0300
    
        android: moved QR Code feature to FD screen
    
    Author: Henrique <henrique.faria@baculasystems.com>
    Date:   Sun Jul 7 15:21:17 2019 -0300
    
        android: added GUI to change Log Level (for Tray and FD). Added watcher thread to track FD Process. Removed default DIR and SD.
    
    Author: Henrique <henrique.faria@baculasystems.com>
    Date:   Fri Jul 5 09:14:09 2019 -0300
    
        android: fixed password generation and restricted log to show only last 1000 lines
    
    Author: Henrique <henrique.faria@baculasystems.com>
    Date:   Tue Jul 2 20:48:24 2019 -0300
    
        android: added code to create default resources (monitor, dir, fd, sd) and to change monitor name
    
    Author: Henrique <henrique.faria@baculasystems.com>
    Date:   Wed Jun 26 22:53:18 2019 -0300
    
        android: refactored paths to traces and call to android permissions
    
    Author: Henrique <henrique.faria@baculasystems.com>
    Date:   Tue Jun 25 11:54:33 2019 -0300
    
        android: removed unused GUI code for FD status. Added default bacula-fd.conf
    
    Author: Henrique <henrique.faria@baculasystems.com>
    Date:   Sun Jun 23 22:30:48 2019 -0300
    
        android: added code to start / stop Java FD Service. Refactored code that unpacks Bacula FD
    
    Author: Henrique <henrique.faria@baculasystems.com>
    Date:   Wed Jun 12 14:58:47 2019 -0300
    
        android: added permission to read external storage (sdcard)
    
    Author: Henrique <henrique.faria@baculasystems.com>
    Date:   Mon May 27 16:37:52 2019 -0300
    
        android: fixed showing of bacula-fd.trace
    
    Author: Henrique <henrique.faria@baculasystems.com>
    Date:   Mon May 27 03:50:37 2019 -0300
    
        android: added code to unpack / start / stop bacula-fd
    
    Author: Henrique <henrique.faria@baculasystems.com>
    Date:   Mon May 27 01:18:31 2019 -0300
    
        android: added file-daemon tab
    
    Author: Henrique Faria <henrique.faria@baculasystems.com>
    Date:   Tue Dec 18 10:56:09 2018 +0100
    
        android: Port bacula tray-monitor for Android

---
+++ bacula/src/qt-console/tray-monitor/tray-ui-controller.cpp
@@ -0,0 +1,155 @@
+/*
+   Bacula(R) - The Network Backup Solution
+
+   Copyright (C) 2000-2020 Kern Sibbald
+
+   The original author of Bacula is Kern Sibbald, with contributions
+   from many others, a complete list can be found in the file AUTHORS.
+
+   You may use this file and others of this release according to the
+   license defined in the LICENSE file, which includes the Affero General
+   Public License, v3.0 ("AGPLv3") and some additional permissions and
+   terms pursuant to its AGPLv3 Section 7.
+
+   This notice must be preserved when any source code is
+   conveyed and/or propagated.
+
+   Bacula(R) is a registered trademark of Kern Sibbald.
+*/
+
+#include "tray-ui-controller.h"
+
+TrayUiController::TrayUiController(QObject *parent):
+    QObject(parent),
+    m_monitorName(""),
+    m_infoDialogText(""),
+    m_config(NULL),
+    m_clients(NULL),
+    m_directors(NULL),
+    m_storages(NULL)
+{
+    m_config = &ConfigStorage::getInstance();
+    m_config->dump_storage();
+    m_traceWatcher = new QFileSystemWatcher(this);
+    connect(m_traceWatcher, SIGNAL(fileChanged(QString)), this, SLOT(handleFdTraceChange(QString)), Qt::QueuedConnection);
+    fclose(fopen(AndroidFD::tracePath().toLatin1().constData(), "ab+"));
+    m_traceWatcher->addPath(AndroidFD::tracePath());
+    MONITOR *mon = m_config->getMonitor();
+    m_monitorName = QString(mon->hdr.name);
+    fetchClients();
+    fetchDirectors();
+    fetchStorages();
+}
+
+QString TrayUiController::fetchFile(QString fpath) {
+    QByteArray fileBytes;
+    QFile file(fpath);
+    file.open(QIODevice::ReadOnly);
+    file.seek(file.size() - 20000);
+    fileBytes = file.read(20000);
+    return QString(fileBytes);
+}
+
+void TrayUiController::fetchClients() {
+    QList<QObject *> *clients = fetchResources(R_CLIENT);
+    setClients(clients);
+}
+
+void TrayUiController::fetchDirectors() {
+    QList<QObject *> *directors = fetchResources(R_DIRECTOR);
+    setDirectors(directors);
+}
+
+void TrayUiController::fetchStorages() {
+    QList<QObject *> *storages = fetchResources(R_STORAGE);
+    setStorages(storages);
+}
+
+QList<QObject *> *TrayUiController::fetchResources(rescode resCode) {
+    QList<RESMON *> *resources = m_config->getResources(resCode);
+    QList<QObject *> *models = new QList<QObject*>();
+
+    for(RESMON *res : *resources) {
+        ResourceModel *model = new ResourceModel();
+        model->setResource(res, resCode);
+        models->append(model);
+    }
+
+    delete resources;
+    return models;
+}
+
+void TrayUiController::deleteResource(ResourceModel *resModel) {
+    const char *error_msg = NULL;
+
+    switch(resModel->resource()->code) {
+    case R_CLIENT:
+        m_clients->removeAll(resModel);
+        error_msg = m_config->saveResources(m_clients, m_directors, m_storages);
+        emit clientsChanged();
+        break;
+    case R_DIRECTOR:
+        m_directors->removeAll(resModel);
+        error_msg = m_config->saveResources(m_clients, m_directors, m_storages);
+        emit directorsChanged();
+        break;
+    case R_STORAGE:
+        m_storages->removeAll(resModel);
+        error_msg = m_config->saveResources(m_clients, m_directors, m_storages);
+        emit storagesChanged();
+        break;
+    default:
+        break;
+    }
+}
+
+void TrayUiController::startFileDaemon() {
+    AndroidFD::start();
+}
+
+void TrayUiController::stopFileDaemon() {
+    AndroidFD::stop();
+}
+
+
+QString TrayUiController::getMonitorName() {
+    return m_monitorName;
+}
+
+void TrayUiController::setMonitorName(QString name) {
+    MONITOR *newMon = new MONITOR();
+    const char *err_msg = NULL;
+    newMon->hdr.name = bstrdup(name.toLatin1().constData());
+    err_msg = m_config->saveMonitor(newMon);
+
+    if (err_msg != NULL) {
+        Dmsg1(0, "Error - could not save monitor name - %s\n", err_msg);
+    }
+
+    m_monitorName = name;
+    free(newMon->hdr.name);
+}
+
+void TrayUiController::handleFdTraceChange(QString) {
+    emit fdTraceChanged();
+}
+
+TrayUiController::~TrayUiController()
+{
+    if (m_clients != NULL) {
+        delete m_clients;
+    }
+
+    if (m_directors != NULL) {
+        delete m_directors;
+    }
+
+    if (m_storages != NULL) {
+        delete m_storages;
+    }
+
+    if (m_traceWatcher != NULL) {
+        m_traceWatcher->removePath(AndroidFD::tracePath());
+        delete m_traceWatcher;
+    }
+}
